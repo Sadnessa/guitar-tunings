69cdd8120f53d3f8fc95473550ec15b42a6697a7
Darya Volkova <anrich@bk.ru>
2022-02-22 13:52:29 +0200

    Added new component.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Header></Header>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style lang="scss">
+body {
+  margin: 0px;
+}
+</style>
+
+<style lang="scss" scoped>
+.header {
+  margin-top: 16px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="header">
+  Mememe
+</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  height: 50px;
+  background: red;
+}
+</style>
