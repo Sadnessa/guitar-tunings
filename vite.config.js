18cbecf3cfd9cf6fb26b83a2a9f99a736b84ff2f
Darya Volkova <anrich@bk.ru>
2022-02-23 11:46:56 +0200

    Build v0.2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/guitar-rank/',
+  base: '/guitar-tunings/',
   build: { outDir: './docs' }
 })
